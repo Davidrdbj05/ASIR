27d519b34a40e4d754e560cd17fbb30540ef399f
David Rodríguez <davidrodriguez01@iesamachado.org>
2023-11-27 14:27:06 +0100

    asd

---
+++ PHP/navegador.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos de Datos</title>
+</head>
+<body>
+    <h1>Navegador</h1>
+    <?php
+        $navegador = $_SERVER['HTTP_USER_AGENT'];
+        $ip = $_SERVER['REMOTE_ADDR'];
+        echo "<p>Estas navegando desde " . $navegador . "</p>";
+        echo "<p>Tu IP es " . $ip . "</p>";
+    ?>
+</body>
+</html>
