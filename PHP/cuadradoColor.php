b747891889adc70fe0969aa7e2c2de4647915d54
David Rodríguez <davidrodriguez01@iesamachado.org>
2023-11-28 14:29:39 +0100

    asd

---
+++ PHP/cuadradoColor.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cuadrado aleatorio</title>
+    <style>
+        #caja{
+            width:400px;
+            height:400px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Cuadrado de color aleatorio</h1>
+    <?php
+        $rojo = dechex(rand(0,255));
+        $verde = dechex(rand(0,255));
+        $azul = dechex(rand(0,255));
+        $color = "#" . $rojo . $verde . $azul;
+        echo "<div id='caja' style=background-color:" . $color . "></div>";
+    ?>
+</body>
+</html>
---
+++ PHP/fechaYHora.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fecha y Hora</title>
+    <style>
+        #caja{
+            width:400px;
+            height:400px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Fecha y Hora</h1>
+    <?php
+        date_default_timezone_set('Spain/Madrid');
+        setlocale(LC_ALL, 'es_ES');
+        echo ucwords(strftime("%A %d %B %Y"));
+    ?>
+</body>
+</html>
